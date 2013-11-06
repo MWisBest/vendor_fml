3a59cb1f94f5f529c3c2e9449b867c92c61e6d8b
MWisBest <repinski23@gmail.com>
2013-11-06 15:05:32 -0600

    More new stuffs.

--- config/common.mk
+++ config/common.mk
@@ -2,11 +2,27 @@
 
 SUPERUSER_PACKAGE_PREFIX := com.fml.superuser
 
+# FML init.rc
+PRODUCT_COPY_FILES += \
+    vendor/fml/prebuilt/common/etc/init.fml.rc:root/init.fml.rc
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
 # Superuser
 PRODUCT_PACKAGES += \
     Superuser \
     su
 
-# FML init.rc
-PRODUCT_COPY_FILES += \
-    vendor/fml/prebuilt/common/etc/init.fml.rc:root/init.fml.rc
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.root_access=1
+
+else
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.root_access=0
+
+endif
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+# Enable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
