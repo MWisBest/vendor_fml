89e9672fb10a8ac9ebe10d36765a5b96be9e1e2a
MWisBest <repinski23@gmail.com>
2013-11-07 20:51:46 -0600

    Boot animation

--- config/common.mk
+++ config/common.mk
@@ -3,6 +3,32 @@
 SUPERUSER_EMBEDDED := true
 SUPERUSER_PACKAGE_PREFIX := com.android.settings.fml.superuser
 
+# Boot Animation
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+bootanimation_sizes := $(subst .zip,, $(shell ls vendor/fml/prebuilt/common/bootanimation))
+bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+PRODUCT_BOOTANIMATION := vendor/fml/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip
+
+
 # FML init.rc
 PRODUCT_COPY_FILES += \
     vendor/fml/prebuilt/common/etc/init.fml.rc:root/init.fml.rc
